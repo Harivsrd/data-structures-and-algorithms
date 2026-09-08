1cdc0e0149e10993bf62d568ec02553906d9f6cf
Hari Venkata Satya Rajesh Desineedi <harivsrdesineedi@gmail.com>
2026-09-08 19:22:02 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (22.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,7 +1,6 @@
 class Solution {
     public int countCommas(int n) {
         if(n < 1000) return 0;
-        else if(n < 1000000) return (n - 1000)+1;
-        return 1;
+        return (n - 1000)+1;
     }
 }
